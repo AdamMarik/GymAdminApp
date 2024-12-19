029ded3e37793b35fa85fc402db46ecb3eb13e50
Adam Marik <adamar200@gmail.com>
2024-12-19 05:40:28 +0000

    Aws intergration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,6 +39,9 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
+  config.hosts << "ec2-54-211-231-216.compute-1.amazonaws.com"
+
+
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = "wss://example.com/cable"
